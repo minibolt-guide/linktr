447da6c813346d1c9b3fc7270d9b68580cecb2b4
⚡️2FakTor⚡️ <twofaktor@protonmail.com>
2025-09-21 11:28:17 +0200

    Update NoteParserKind0.js

--- src/components/NoteParserKind0.js
+++ src/components/NoteParserKind0.js
@@ -29,7 +29,7 @@ const NoteParserKind0 = ({ note }) => {
         <a href={`lightning:${lud16}`} target='_blank' rel='noreferrer'>
           <LnIcon className='svg-src' />
         </a>
-        <a href={process.env.REACT_APP_NOSTR_OUTER_CHAT + nPub} target='_blank' rel='noreferrer'>
+        <a href={process.env.REACT_APP_NOSTR_OUTER_CHAT + 763b053bc37a0924c16f8f5a60782b7c1ad0a95b9bcac67971b355994e8daf9f} target='_blank' rel='noreferrer'>
           <GoChat className='svg-src' />
         </a>
         <a href={process.env.REACT_APP_NOSTR_OUTER_PROFILES + nPub} target='_blank' rel='noreferrer'>
